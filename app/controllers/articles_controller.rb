c93a9b72b6bc7eb3be29ba58cae7efdfa52288db
KnighT178 <egor.skandakov@gmail.com>
2022-10-09 19:07:33 +0300

    Added bootstrap, fixed Vue, added controller for Article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def show
+
+  end
+end
