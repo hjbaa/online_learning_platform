84b407b549f570ae5fc1d83459dfcef7cdc648c6
KnighT178 <egor.skandakov@gmail.com>
2022-12-03 16:41:50 +0300

    Added dependent nullify when destroying group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   belongs_to :major_teacher, class_name: 'Teacher'
   has_and_belongs_to_many :teachers, join_table: 'groups_users', association_foreign_key: 'user_id'
 
-  has_many :students
+  has_many :students, dependent: :nullify
 
   validates_presence_of :name
 end
