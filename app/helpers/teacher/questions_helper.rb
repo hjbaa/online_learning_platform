c032c231e43c7f0dc8a71aaee43748811f4fd385
KnighT178 <egor.skandakov@gmail.com>
2022-12-11 21:41:23 +0300

    Added display of questions on the page with the test

--- app/helpers/teacher/questions_helper.rb
+++ app/helpers/teacher/questions_helper.rb
@@ -6,4 +6,15 @@ module Teacher::QuestionsHelper
       ['No option question', 'NoOptionQuestion']
     ]
   end
+
+  def type(question)
+    case question.type
+    when 'MultipleOptionQuestion'
+      'Question with multiple choice'
+    when 'SingleOptionQuestion'
+      'Question with single choice'
+    else
+      'Question without choice'
+    end
+  end
 end
